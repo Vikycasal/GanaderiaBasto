ecc811997480a5492aee3cf8ef1508fd9e7615fa
victoria.casalf@gmail.com <victoria.casalf@gmail.com>
2022-06-09 21:28:17 -0300

    rutas listas

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,21 @@ import ListEstablishment from "./ListEstablishment"
 import AddEstablishment from './AddEstablishment';
 import EditEstablishment from "./EditEstablishment"
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+//Once the components are finished, I start to build the routes, first I install react-router-dom
 
 function App() {
   return (
     <div className="App">
        <h1>Ganaderia Basto</h1>
-       <ListEstablishment />
-       <AddEstablishment />
-       <EditEstablishment />
-       
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ListEstablishment />}/>
+        <Route path="/addestablishment" element={<AddEstablishment />}/>
+        <Route path="/editestablishment" element={<EditEstablishment />}/>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
